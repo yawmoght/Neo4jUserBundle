178a3aa99dc404f85874d826601b542ccb908844
frne <fneff89@gmail.com>
2014-04-06 17:15:28 +0200

    Fixed array notation

--- Tests/Entity/UserTest.php
+++ Tests/Entity/UserTest.php
@@ -9,7 +9,7 @@ class UserTest extends \PHPUnit_Framework_TestCase
 
     public function testImplementsUserInterface()
     {
-        $user = new User('johndoe', '1234', 'sfo7sdgfos9d7gfos9dfg', ['ROLE_USER', 'ROLE_ADMIN']);
+        $user = new User('johndoe', '1234', 'sfo7sdgfos9d7gfos9dfg', array('ROLE_USER', 'ROLE_ADMIN'));
         $this->assertInstanceOf('Symfony\Component\Security\Core\User\UserInterface', $user);
     }
 }
