035f041255a69b3b07e71e9efe6210e7b6d14884
frne <fneff89@gmail.com>
2014-04-07 20:29:44 +0200

    Some more swag for the create-user command

--- Command/CreateUserCommand.php
+++ Command/CreateUserCommand.php
@@ -2,47 +2,88 @@
 
 namespace Frne\Bundle\Neo4jUserBundle\Command;
 
+use Frne\Bundle\Neo4jUserBundle\Model\UserManager;
+use HireVoice\Neo4j\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateUserCommand extends ContainerAwareCommand
 {
+    /**
+     * @var UserManager
+     */
+    private $userManager;
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * Creepy workaround for testing the command
+     *
+     * @param UserManager $userManager
+     * @param EntityManager $em
+     */
+    public function setDeps(UserManager $userManager, EntityManager $em)
+    {
+        $this->userManager = $userManager;
+        $this->em = $em;
+    }
+
     protected function configure()
     {
         $this
             ->setName('neo4j:user-bundle:create-user')
-            ->setDescription('Create a user');
+            ->setDescription('Create a user')
+            ->addOption('username', 'u', InputOption::VALUE_REQUIRED, 'The username to be set')
+            ->addOption('password', 'p', InputOption::VALUE_REQUIRED, 'The password of the user')
+            ->addOption(
+                'roles',
+                'r',
+                InputOption::VALUE_REQUIRED,
+                'Roles to assign to the created user (comma separated)'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $userManager = $this->getContainer()->get('neo4j.user_manager');
-        $em = $this->getContainer()->get('neo4j.manager');
-        $dialog = $this->getHelperSet()->get('dialog');
+        if (!$username = $input->getOption('username')) {
+            throw new \InvalidArgumentException('Username must be set!');
+        }
 
-        $username = $dialog->ask(
-            $output,
-            'Username: '
-        );
+        if (!$password = $input->getOption('password')) {
+            throw new \InvalidArgumentException('Password must be set!');
+        }
 
-        $password = $dialog->ask(
-            $output,
-            'Password: '
-        );
+        $userManager = ($this->userManager instanceof UserManager) ?
+            $this->userManager :
+            $this->getContainer()->get('neo4j.user_manager');
+
+        $em = ($this->em instanceof EntityManager) ?
+            $this->em :
+            $this->getContainer()->get('neo4j.manager');
+
+        $roleString = $input->getOption('roles');
+        $roles = (is_string($roleString)) ?
+            explode(',', $roleString) :
+            array('ROLE_USER');
 
         $user = $userManager->createUser();
 
         $user
             ->setUsername($username)
-            ->setPlainPassword($password)
-            ->setSalt($userManager->generateSalt());
+            ->setPlainPassword($password = $input->getOption('password'))
+            ->setSalt($userManager->generateSalt())
+            ->setRoles($roles);
 
         $userManager->updatePassword($user);
 
         $em->persist($user);
         $em->flush();
 
-        $output->writeln("Created user '$username' with password '$password'");
+        $output->writeln("Created user '$username' with password '$password' and roles '$roleString'");
     }
 }
